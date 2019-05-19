577a6f30e04adb8745100d200bbaa34bfe6543d5
ohmayr <omairnaveed@ymail.com>
2019-05-19 11:35:20 -0400

    api added

--- app.js
+++ app.js
@@ -13,12 +13,27 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
-const getApiAndEmit = "TODO";
+const getApiAndEmit = async socket => {
+    try {
+        const res = await axios.get(
+            "https://api.darksky.net/forecast/b48d30378dfe9259fa9dd55998b5cc2b/43.7695,11.2558"
+        ); // Getting the data from DarkSky
+        socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+};
+
+let interval;
 
 io.on("connection", socket => {
-    console.log("New client connected"), setInterval(
-        ()=> getApiAndEmit(socket),
-        10000
-    );
-    socket.on("disconnect", () => console.log("Client disconnected"));
+    console.log("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => { getApiAndEmit(socket), 10000 });
+
+    socket.on("disconnect", () => { console.log("Client disconnected") });
 });
+
+server.listen(port, () => { console.log(`Listening on port ${port}`) });
